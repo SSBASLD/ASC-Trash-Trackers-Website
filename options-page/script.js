265abb528903fd7839093e5b69e0147de66eeba1
SSBASLD <77936828+SSBASLD@users.noreply.github.com>
2024-07-04 15:13:01 -0500

    basic version finished

--- options-page/script.js
+++ options-page/script.js
@@ -2,3 +2,13 @@ let gameButton = document.getElementById("GameButton");
 gameButton.onclick = (e) => {
     window.location.href = window.location.href.replace("/options-page/index.html", "") + "/game/index.html";
 }
+
+let timeout = setTimeout(() => {
+    window.location.href = window.location.href.replace('/options-page/index.html', "") + "/index.html";
+  }, 30000);
+
+document.onclick = () => {
+    timeout = setTimeout(() => {
+        window.location.href = window.location.href.replace('/options-page/index.html', "") + "/index.html";
+      }, 30000);
+}
